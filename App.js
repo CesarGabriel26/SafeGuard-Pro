63e510b9797b6e512425d759e98f000be7320148
Cesar Gabriel <cesar.oliveira@yes.technology>
2024-03-19 13:39:59 -0300

    inicio 19/03/2024

--- App.js
+++ App.js
@@ -1,5 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
